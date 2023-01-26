735ca5ebc2f8348a918beeb7d83e76f347fc16b2
beardyface <henryamwilliams@gmail.com>
2023-01-26 15:45:50 +1300

    Minor changes

--- scripts/Camera.py
+++ scripts/Camera.py
@@ -13,18 +13,14 @@ from pathlib import Path
 home = str(Path.home())
 
 class Camera(object):
-
-    def __init__(self, camera_id=0):  # 0 is usb camera, 1 is laptop camera (when the usb camera is plugged in )
+    def __init__(self, camera_id=0):
 
         # connecting to the camera takes the longest
         self.camera = cv2.VideoCapture(camera_id)
         if not self.camera.isOpened():
             raise Exception("Could not open video device")
 
-        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)  # aruco dictionary
-        self.aruco_params = cv2.aruco.DetectorParameters_create()
-        self.marker_size = 18  # mm
-        #TODO: make this relative
+        #TODO: make this relative and parameters into the camera object
         self.camera_matrix = np.loadtxt((home+"/gripperCode/Gripper-Code/scripts/config/camera_matrix.txt"))
         self.camera_distortion = np.loadtxt((home+"/gripperCode/Gripper-Code/scripts/config/camera_distortion.txt"))
 
