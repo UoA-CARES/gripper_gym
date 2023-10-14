5788d63c9c08a2fcc703ae9af0b4d4edebe33722
Tony Cui <ycui982@aucklanduni.ac.nz>
2023-10-15 11:42:17 +1300

    feat: Clean up graphing file

--- scripts/graph_results.py
+++ scripts/graph_results.py
@@ -5,22 +5,21 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from argparse import ArgumentParser
-import plotly.graph_objects as go
 import collections
 import pathlib
 
 Z = 1.960 # 95% confidence interval
 X_VALS_FILE_NAME = "steps_per_episode.txt"
-Y_VALS_FILE_NAME = "success_list.txt"
+SUCCESS_FILE = "success_list.txt"
+REWARD_FILE = "reward.txt"
 X_LIMIT = 30000
+CURRENT_DIR = os.getcwd()
+GRAPHS_DIR = os.path.join(os.path.dirname(CURRENT_DIR), 'graphs')
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--folder1",      type=str)
-    parser.add_argument("--folder2",      type=str)
-    parser.add_argument("--folder3",      type=str)
+    parser.add_argument("--folder_path",      type=str)
     parser.add_argument("--plot_type",      type=str)
-    parser.add_argument("--title",      type=str)
     return parser.parse_args()
 
 def parse_step(step_file, arr):
@@ -51,160 +50,128 @@ def parse_folder(folder, folders_arr):
         sub_dirs = [f.path for f in os.scandir(folder) if f.is_dir()]
         folders_arr.append(sub_dirs)
 
-def plot_single_graph(datas_map, title, window_size=50):
-    plt.ioff()
-    figure = plt.figure()
-    figure.set_figwidth(6)
-    sns.set_theme(style="darkgrid")
+def plot_reward(datas_map_arr, titles, window_size=20):
+    """
+    Plot rewards with confidence intervals.
 
-    for key in datas_map:
-        x_label = "x"
-        y_label = "y"
-
-        df = pd.DataFrame({'x' : datas_map[key]['x'], 'y': datas_map[key]['y']})
-
-        # confidence interval stuff
-        df["avg"] = df[y_label].rolling(window=window_size, min_periods=1).mean()
-        movStd = df[y_label].rolling(window=window_size, min_periods=1).std()
-
-        confIntPos = df["avg"] + Z * movStd / np.sqrt(window_size)
-        confIntNeg = df["avg"] - Z * movStd / np.sqrt(window_size)
-
-        ax = sns.lineplot(data=df, x=x_label, y="avg", label=key)
-
-        # ax.set_xlim(1,12000) # Limit graph to specific x value
-
-        ax.set(xlabel=x_label, ylabel="avg")
-        plt.fill_between(df[x_label], confIntNeg, confIntPos, alpha=0.2)
-
-    sns.move_legend(ax, "lower right")
-    plt.xlabel("Steps")
-    plt.ylabel("Average Reward")
-    plt.title(title)
-    plt.show()
-
-def plot_algo_compare(datas_map_arr, titles, window_size=20):
+    Args:
+    datas_map_arr (list): List of data maps.
+    titles (list): List of titles for subplots.
+    window_size (int, optional): Window size for rolling mean and standard deviation. Defaults to 20.
+    """
     plt.ioff()
     sns.set()
     fig, axes = plt.subplots(1, len(datas_map_arr), constrained_layout=True)
     sns.set_theme(style="darkgrid")
 
     for i, datas_map in enumerate(datas_map_arr):
-        for key in datas_map:
+        for key, data in datas_map.items():
             x_label = "x"
             y_label = "y"
 
-            df = pd.DataFrame({'x' : datas_map[key]['x'], 'y': datas_map[key]['y']})
+            df = pd.DataFrame({'x': data['x'], 'y': data['y']})
 
-            # confidence interval stuff
+            # Calculate confidence intervals
             df["avg"] = df[y_label].rolling(window=window_size, min_periods=1).mean()
-            movStd = df[y_label].rolling(window=window_size, min_periods=1).std()
+            mov_std = df[y_label].rolling(window=window_size, min_periods=1).std()
 
-            confIntPos = df["avg"] + Z * movStd / np.sqrt(window_size)
-            confIntNeg = df["avg"] - Z * movStd / np.sqrt(window_size)
+            conf_int_pos = df["avg"] + Z * mov_std / np.sqrt(window_size)
+            conf_int_neg = df["avg"] - Z * mov_std / np.sqrt(window_size)
 
+            # Plotting
             axes[i] = sns.lineplot(ax=axes[i], data=df, x=x_label, y="avg", label=key)
-            axes[i].set_xlim(1,X_LIMIT) # Limit graph to specific x value
-            axes[i].fill_between(df[x_label], confIntNeg, confIntPos, alpha=0.2)
-            
+            axes[i].set_xlim(1, X_LIMIT)  # Limit graph to specific x value
+            axes[i].fill_between(df[x_label], conf_int_neg, conf_int_pos, alpha=0.2)
+
         axes[i].set_title(titles[i])
         axes[i].set_xlabel("Steps")
         axes[i].set_ylabel("")
-        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))  # Set x-axis tick labels in scientific notation
+        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0, 0))  # Set x-axis tick labels in scientific notation
 
         sns.move_legend(axes[i], "lower right")
 
     axes[0].set_ylabel('Average Reward')
+    fig.set_size_inches(16, 5)
+
+    # Define the path for saving the figure
+    figure_path = os.path.join(GRAPHS_DIR, 'reward_plot.png')
+    plt.savefig(figure_path)  # Save the figure
     plt.show()
 
 def plot_success_rate(datas_map_arr, titles, window_size=100):
+    """
+    Plot success rates with confidence intervals.
+
+    Args:
+    datas_map_arr (list): List of data maps.
+    titles (list): List of titles for subplots.
+    window_size (int, optional): Window size for rolling mean and standard deviation. Defaults to 100.
+    """
     plt.ioff()
     sns.set()
     fig, axes = plt.subplots(1, len(datas_map_arr), constrained_layout=True)
     sns.set_theme(style="darkgrid")
 
     for i, datas_map in enumerate(datas_map_arr):
-        for key in datas_map:
+        for key, data in datas_map.items():
             x_label = "x"
             y_label = "y"
 
-            df = pd.DataFrame({'x' : datas_map[key]['x'], 'y': datas_map[key]['y']})
+            df = pd.DataFrame({'x': data['x'], 'y': data['y']})
 
-            # confidence interval stuff
+            # Calculate confidence intervals
             df["avg"] = df[y_label].rolling(window=window_size, min_periods=1).mean()
-            movStd = df[y_label].rolling(window=window_size, min_periods=1).std()
+            mov_std = df[y_label].rolling(window=window_size, min_periods=1).std()
 
-            confIntPos = df["avg"] + Z * movStd / np.sqrt(window_size)
-            confIntNeg = df["avg"] - Z * movStd / np.sqrt(window_size)
+            conf_int_pos = df["avg"] + Z * mov_std / np.sqrt(window_size)
+            conf_int_neg = df["avg"] - Z * mov_std / np.sqrt(window_size)
 
+            # Plotting
             axes[i] = sns.lineplot(ax=axes[i], data=df, x=x_label, y="avg", label=key)
-            axes[i].set_xlim(1,X_LIMIT) # Limit graph to specific x value
-            axes[i].fill_between(df[x_label], confIntNeg, confIntPos, alpha=0.2)
-            
+            axes[i].set_xlim(1, X_LIMIT)  # Limit graph to specific x value
+            axes[i].fill_between(df[x_label], conf_int_neg, conf_int_pos, alpha=0.2)
+
         axes[i].set_title(titles[i])
         axes[i].set_xlabel("Steps")
         axes[i].set_ylabel("")
-        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))  # Set x-axis tick labels in scientific notation
+        axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0, 0))  # Set x-axis tick labels in scientific notation
 
         sns.move_legend(axes[i], "lower right")
 
     axes[0].set_ylabel('Success Rate')
+    fig.set_size_inches(16, 5)
+
+    # Define the path for saving the figure
+    figure_path = os.path.join(GRAPHS_DIR, 'success_rate_plot.png')
+    plt.savefig(figure_path)  # Save the figure
     plt.show()
 
-# def plot_G_values(folder1, folder2, folder3):
-#     folders = []
-#     task_to_algos = {}
-#     titles = ["TD3 30-330", "SAC 30-330", "DDPG 30-330"]
-
-#     parse_folder(folder1, folders)
-#     parse_folder(folder2, folders)
-#     parse_folder(folder3, folders)
-
-#     for sub_dirs in folders:
-#         datas_map = {}
-
-#         for sub_dir in sub_dirs:
-#             folder_name = os.path.basename(sub_dir)
-#             splitted = folder_name.split("_")
-#             algorithm = splitted[-2]
-
-#             # Regex to extract G value at end
-#             pattern = r"G\d+"
-#             matches = re.findall(pattern, folder_name)
-
-#             if matches:
-#                 g_val = matches[0][1:]
-#             else:
-#                 g_val = "error"
-
-#             step_file = f"{sub_dir}/data/{X_VALS_FILE_NAME}"
-#             reward_file = f"{sub_dir}/data/{Y_VALS_FILE_NAME}"
-
-#             key = algorithm + " G:" + g_val
-#             datas_map[key] = { "x": [] , "y": []}
-#             parse_step(step_file, datas_map[key]["x"])
-#             parse_reward(reward_file, datas_map[key]["y"])
-
-#         od = collections.OrderedDict(sorted(datas_map.items()))
-
-#         task_name = os.path.basename(sub_dir)
-#         task_to_algos[task_name] = od
-    
-#     print(type(task_to_algos))
-#     plot_multiple(task_to_algos, titles)
-
 def plot_training_evaluation(root_folder):
+    """
+    Plot training evaluation data from the given root folder.
+
+    Args:
+    root_folder (str): Root folder path.
+
+    This function reads evaluation data from different algorithm folders within each task folder and plots the data accordingly.
+    """
     dataframes = []
     titles = []
 
-    for task_folder in pathlib.Path(root_folder).iterdir():
-        if not task_folder.is_dir(): pass
+    # Sort the folders in numerical order
+    sorted_task_folders = sorted(pathlib.Path(root_folder).iterdir())
+
+    for task_folder in sorted_task_folders:
+        if not task_folder.is_dir():
+            continue
+
         datas_map = {}
         for algo_folder in pathlib.Path(task_folder).iterdir():
-            if not algo_folder.is_dir(): pass
+            if not algo_folder.is_dir():
+                continue
+
             folder_name = os.path.basename(algo_folder)
-            splitted = folder_name.split("_")
-            algorithm = splitted[-1]
+            algorithm = folder_name.split("_")[-1]
 
             csv_file = f"{algo_folder}/data/{folder_name}_evaluation"
             df = pd.read_csv(csv_file)
@@ -214,13 +181,20 @@ def plot_training_evaluation(root_folder):
         od = collections.OrderedDict(sorted(datas_map.items()))
         dataframes.append(od)
 
-        splitted = os.path.basename(task_folder).split("_")
-        task = splitted[-1]
-        titles.append(f"{task}")
+        task = os.path.basename(task_folder).split("_")[-1]
+        titles.append(f"Rotation {task}")
 
     plot_evals(dataframes, titles)
 
 def plot_evals(dataframes, titles, window_size=20):
+    """
+    Plot evaluation data.
+
+    Args:
+    dataframes (list): List of dataframes.
+    titles (list): List of titles for subplots.
+    window_size (int, optional): Window size for rolling mean and standard deviation. Defaults to 20.
+    """
     plt.ioff()
     sns.set()
     fig, axes = plt.subplots(1, len(dataframes), constrained_layout=True)
@@ -235,14 +209,14 @@ def plot_evals(dataframes, titles, window_size=20):
 
             # confidence interval stuff
             df["avg"] = df[y_label].rolling(window=window_size, min_periods=1).mean()
-            movStd = df[y_label].rolling(window=window_size, min_periods=1).std()
+            mov_std = df[y_label].rolling(window=window_size, min_periods=1).std()
 
-            confIntPos = df["avg"] + Z * movStd / np.sqrt(window_size)
-            confIntNeg = df["avg"] - Z * movStd / np.sqrt(window_size)
+            conf_int_pos  = df["avg"] + Z * mov_std / np.sqrt(window_size)
+            conf_int_neg  = df["avg"] - Z * mov_std / np.sqrt(window_size)
 
             axes[i] = sns.lineplot(ax=axes[i], data=df, x=x_label, y="avg", label=key)
             axes[i].set_xlim(1,X_LIMIT) # Limit graph to specific x value
-            axes[i].fill_between(df[x_label], confIntNeg, confIntPos, alpha=0.2)
+            axes[i].fill_between(df[x_label], conf_int_neg, conf_int_pos, alpha=0.2)
             
         axes[i].set_title(titles[i])
         axes[i].set_xlabel("Steps")
@@ -252,111 +226,90 @@ def plot_evals(dataframes, titles, window_size=20):
         sns.move_legend(axes[i], "lower right")
 
     axes[0].set_ylabel('Average Reward')
+    fig.set_size_inches(16, 5)
+
+    # Define the path for saving the figure
+    figure_path = os.path.join(GRAPHS_DIR, 'training_evaluation_plot.png')
+    plt.savefig(figure_path)  # Save the figure
     plt.show()
 
-def plot_different_algorithms(folder1, success_rate):
+def plot_different_algorithms(folder, success_rate):
+    """
+    Plot different algorithms based on the data in the provided folder.
+
+    Args:
+    folder (str): Path to the folder containing the data.
+    success_rate (bool): Boolean indicating whether to plot success rates or rewards.
+
+    This function reads data from different algorithm folders within each task folder and plots the data based on the type of plot specified.
+    """
     datas_map_arr = []
     titles = []
 
-    for task_folder in pathlib.Path(folder1).iterdir():
-        if not task_folder.is_dir(): pass
+    # Sort the folders in numerical order
+    sorted_task_folders = sorted(pathlib.Path(folder).iterdir())
+
+    for task_folder in sorted_task_folders:
+        if not task_folder.is_dir():
+            continue
+
         datas_map = {}
         for algo_folder in pathlib.Path(task_folder).iterdir():
-            if not algo_folder.is_dir(): pass
+            if not algo_folder.is_dir():
+                continue
+
             folder_name = os.path.basename(algo_folder)
-            splitted = folder_name.split("_")
-            algorithm = splitted[-1]
+            algorithm = folder_name.split("_")[-1]
 
             step_file = f"{algo_folder}/data/{X_VALS_FILE_NAME}"
-            reward_file = f"{algo_folder}/data/{Y_VALS_FILE_NAME}"
+            if success_rate:
+                vals_file = f"{algo_folder}/data/{SUCCESS_FILE}"
+            else:
+                vals_file = f"{algo_folder}/data/{REWARD_FILE}"
 
-            datas_map[algorithm] = { "x": [] , "y": []}
+            datas_map[algorithm] = {"x": [], "y": []}
             parse_step(step_file, datas_map[algorithm]["x"])
-            parse_reward(reward_file, datas_map[algorithm]["y"])
+            parse_reward(vals_file, datas_map[algorithm]["y"])
 
         od = collections.OrderedDict(sorted(datas_map.items()))
         datas_map_arr.append(od)
 
-        splitted = os.path.basename(task_folder).split("_")
-        task = splitted[-1]
-        titles.append(f"{task}")
-    
-    if (success_rate):
+        task = os.path.basename(task_folder).split("_")[-1]
+        titles.append(f"Rotation {task}")
+
+    if success_rate:
         plot_success_rate(datas_map_arr, titles)
     else:
-        plot_algo_compare(datas_map_arr, titles)
+        plot_reward(datas_map_arr, titles)
 
-def plot_single(folder1, title, plot_type):
+def create_graphs_dir(graphs_dir):
+    """
+    Create the 'graphs' directory if it does not already exist.
 
-    sub_dirs = [f.path for f in os.scandir(folder1) if f.is_dir()]
-    datas_map = {}
-    for sub_dir in sub_dirs:
-        folder_name = os.path.basename(sub_dir)
-        splitted = folder_name.split("_")
-        algorithm = splitted[-2]
-
-        if plot_type == "g_single":
-            # Regex to extract G value at end
-            pattern = r"G\d+"
-            matches = re.findall(pattern, folder_name)
-
-            if matches:
-                g_val = matches[0][1:]
-            else:
-                g_val = "error"
-
-            step_file = f"{sub_dir}/data/{X_VALS_FILE_NAME}"
-            reward_file = f"{sub_dir}/data/{Y_VALS_FILE_NAME}"
-            key = algorithm + " G:" + g_val
-        else:
-            key = algorithm
-
-        step_file = f"{sub_dir}/data/{X_VALS_FILE_NAME}"
-        reward_file = f"{sub_dir}/data/{Y_VALS_FILE_NAME}"
-
-        datas_map[key] = { "x": [] , "y": []}
-        parse_step(step_file, datas_map[key]["x"])
-        parse_reward(reward_file, datas_map[key]["y"])
-
-    od = collections.OrderedDict(sorted(datas_map.items()))
-    
-    plot_single_graph(od, title)
-
-def plot_evaluation(folder1, title):
-    # sub_dirs = [f.path for f in os.scandir(folder1) if f.is_dir()]
-    datas_map = {}
-    folder_name = os.path.basename(folder1)
-    splitted = folder_name.split("_")
-    algorithm = splitted[-2]
-
-    step_file = f"{folder1}/data/{X_VALS_FILE_NAME}"
-    reward_file = f"{folder1}/data/{Y_VALS_FILE_NAME}"
-
-    key = algorithm
-
-    datas_map[key] = { "x": [] , "y": []}
-    parse_step(step_file, datas_map[key]["x"])
-    parse_reward(reward_file, datas_map[key]["y"])
-
-    od = collections.OrderedDict(sorted(datas_map.items()))
-    
-    plot_single_graph(od, title)
+    Args:
+    graphs_dir (str): Directory path for saving graphs.
+    """
+    if not os.path.exists(graphs_dir):
+        os.makedirs(graphs_dir)
+        print(f"Directory '{graphs_dir}' created successfully.")
+    else:
+        print(f"Directory '{graphs_dir}' already exists.")
 
 # plot graphs with desired comparison type
 def main():
+    """
+    Main function to execute the script for plotting graphs.
+    """
     args = parse_args()
-    # if (args.plot_type == "g"):
-    #     plot_G_values(args.folder1, args.folder2, args.folder3)
-    if (args.plot_type == "algorithms"):
-        plot_different_algorithms(args.folder1, success_rate=False)
-    if (args.plot_type == "success"):
-        plot_different_algorithms(args.folder1, success_rate=True)
-    elif (args.plot_type == "g_single" or args.plot_type == "algorithms_single"):
-        plot_single(args.folder1, args.title, args.plot_type)
-    elif (args.plot_type == "post_training_evaluation"):
-        plot_evaluation(args.folder1, args.title)
-    elif (args.plot_type == "training_evaluation"):
-        plot_training_evaluation(args.folder1)
+    graphs_dir = os.path.join(os.path.dirname(os.getcwd()), 'graphs')
+    create_graphs_dir(graphs_dir)
+    
+    if args.plot_type == "reward":
+        plot_different_algorithms(args.folder_path, success_rate=False)
+    elif args.plot_type == "success_rate":
+        plot_different_algorithms(args.folder_path, success_rate=True)
+    elif args.plot_type == "training_evaluation":
+        plot_training_evaluation(args.folder_path)
     else:
         raise ValueError("Invalid plot type")
 
